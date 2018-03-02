1cde6862da8099c36b0077f65816aa22cc2ed921
wangpeng999 <1195490258@qq.com>
2018-03-02 10:42:10 +0800

    new pojo

---
+++ SpringMVC01/src/com/wangpeng/po/teacher.java
@@ -0,0 +1,11 @@
+package com.wangpeng.po;
+
+import java.util.Date;
+
+public class teacher {
+	private int id;
+	private String username;// 用户姓名
+	private String sex;// 性别
+	private Date birthday;// 生日
+	private String address;// 地址
+}
